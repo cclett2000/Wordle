17d82449b7f90f74cb0a57b8328fced5e7b2ebda
Charles Lett Jr <cclett2000@gmail.com>
2022-08-16 21:32:57 -0500

    Version 1.2 -- implemented better UI logic and fixed several bugs

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -16,40 +16,63 @@ class MainActivity : AppCompatActivity() {
 
         val wordToGuess = FourLetterWordList.getRandomFourLetterWord();     // get word to check
 
-        val guessesField = findViewById<TextView>(R.id.guesses_view)
-        val failView = findViewById<TextView>(R.id.fail_view)
-        val debugView = findViewById<TextView>(R.id.DEBUG_view_word)
+        val guessesField = findViewById<TextView>(R.id.guesses_view)        // guess results display
+        val failView = findViewById<TextView>(R.id.fail_view)               // fail view -- basically info view, just hate refactoring
+        val debugView = findViewById<TextView>(R.id.DEBUG_view_word)        // guess word display -- used to be debug, still hate refactoring
         val getUserInput = findViewById<Button>(R.id.btn_get_user_input);   // button to get user input
         val editText = findViewById<EditText>(R.id.et_user_input);          // user input text
 
-        var guessLimit = 3
-        var checkGuessDisplay = ""
-        var userInputDisplay = ""
+        var guessLimit = 3                                                  // set number of attempts
+        var checkGuessDisplay = ""                                          // used to display check results
+        var userInputDisplay = ""                                           // used to display user input
 
+        // button logic
         getUserInput.setOnClickListener {
+            // guesses remaining check
             if (guessLimit != 0) {
-                val editTextString = editText.text.toString().uppercase(Locale.getDefault());
-                val result = checkGuess(editTextString, wordToGuess)
-                checkGuess(editTextString, wordToGuess)
+                var isCorrect = false       // correct guess flag
+                val editTextString = editText.text.toString().uppercase(Locale.getDefault());   // to string for edit text
+                if (editTextString.length == 4) {
+                    failView.text = ""
+                    val result = checkGuess(editTextString, wordToGuess)
+                    checkGuess(editTextString, wordToGuess)
 
-                if (checkGuessDisplay == "") {
-                    checkGuessDisplay = result
-                    userInputDisplay = editTextString
-                } else {
-                    checkGuessDisplay = checkGuessDisplay + "\n" + result
-                    userInputDisplay = userInputDisplay + "\n" + editTextString
+                    // display conditional
+                    // first guess
+                    if (checkGuessDisplay == "") {
+                        checkGuessDisplay = result
+                        userInputDisplay = editTextString
+                    }
+                    // following guesses
+                    else {
+                        checkGuessDisplay = checkGuessDisplay + "\n" + result
+                        userInputDisplay = userInputDisplay + "\n" + editTextString
+                    }
+
+                    guessesField.text = checkGuessDisplay
+                    debugView.text = userInputDisplay
+
+                    // on win conditional
+                    if (result == "OOOO") {
+                        failView.text = "Congratz! You've Guessed Correctly!\nAnswer: $wordToGuess"
+                        isCorrect = true
+                        guessLimit = 0
+                    } else {
+                        guessLimit--
+
+                        // on lost conditional
+                        if (guessLimit == 0 && !isCorrect)
+                            failView.text = "Oops! You're Out of Attempts!\nAnswer: $wordToGuess"
+                    }
                 }
-
-                guessesField.text = checkGuessDisplay
-                debugView.text = userInputDisplay
-                guessLimit--
-
-                if (guessLimit == 0) failView.text =
-                    "Oops! You're Out of Attempts!\nCorrect Answer: $wordToGuess"
+                // invalid input conditional
+                else
+                    failView.text = "Oops, Gotta enter a Four Letter Word!"
             }
         }
     }
 
+    // method for checking guess
     private fun checkGuess(guess: String, wordToGuess: String) : String {
         var result = ""
         for (i in 0..3) {
